d47f228923acd6cf7b1705e8a034c3ea764e89ea
LMKH <131709088+LMKH@users.noreply.github.com>
2023-06-14 12:02:01 +0100

    Add files via upload

---
+++ task_1_hello_world.py
@@ -0,0 +1,9 @@
+user_input_name = input("Please enter your name: ")
+
+print("Hello, " + user_input_name)
+
+user_input_age = input("What is your age? ")
+
+print("You are " + user_input_age + " years old")
+print()
+print("Hello World")
